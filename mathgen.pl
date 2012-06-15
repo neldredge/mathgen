eef97ead1fb2b712581d9f767fcc79959a94bd1c
Nate Eldredge <nate@thatsmathematics.com>
2012-06-15 12:56:28 -0400

    Initial commit

---
+++ mathgen.pl
@@ -0,0 +1,344 @@
+#!/usr/bin/perl -w
+
+#    This file is part of Mathgen.
+#
+#    Mathgen is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    Mathgen is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Mathgen; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+use strict;
+use scigen;
+use Getopt::Long;
+use File::Temp qw { tempdir };
+
+my $default_mode = 'view';
+my $default_viewer = 'evince';
+my $default_product = 'article';
+
+my %modes = map {$_ => 1} qw { pdf zip dir view raw };
+my %products = (
+    'article' => { PRETTY => 'latex' },
+    'book' => { PRETTY => 'latexbook' },
+    'blurb' => { PRETTY => 0 }
+    );
+
+my $bibname = "scigenbibfile.bib";
+
+my $data_dir = '.';
+
+
+
+sub usage {
+    select(STDERR);
+    print <<EOUsage;
+    
+$0 [options]
+  Options:
+
+    --help                    Display this help message
+    --author <quoted_name>    An author of the paper (can be specified 
+                              multiple times)
+                              Default: One random author
+    --mode pdf|zip|dir|view|raw
+                              What to output
+    	   		      pdf: Output a PDF file
+			      zip: Output a zip file with 
+			      	   LaTeX/BiBTeX source and PDF
+			      dir: Leave source and PDF in directory
+			       	   specified with --dir
+			      view: Invoke viewer on PDF file
+			      raw: Output raw tex/txt only (required for blurb)
+    --viewer <prog>	      Use <prog> as PDF viewer (default: $default_viewer)
+    --dir <dir>		      For --mode=dir: use <dir> for output
+    --output <file>	      For --mode=pdf,zip,raw: Output to <file>
+    	     		      Use - for stdout
+    --product article|book|blurb
+                              What to generate (default: $default_product)
+    --seed <seed>             Use <seed> to seed the PRNG
+    --debug                   Enable various debugging features
+EOUsage
+    exit(1);
+
+}
+
+my @authors;
+my $mode = $default_mode; # pdf, zip, dir, view, raw (undocumented)
+my $viewer = $default_viewer;
+my $dir;
+my $output;
+my $product = $default_product;
+my $seed;
+my $debug = 0;
+
+my %options;
+GetOptions( \%options, 
+	    "help|?" => \&usage,
+	    "author=s@" => \@authors, 
+	    "mode=s" => \$mode,
+	    "viewer=s" => \$viewer,
+	    "dir=s" => \$dir,
+	    "output=s" => \$output,
+	    "product=s" => \$product,
+	    "seed=i" => \$seed,
+	    "debug!" => \$debug)
+    or usage();
+
+if (!$modes{$mode}) {
+    printf STDERR "$0: Unknown mode $mode\n";
+    usage();
+}
+
+if (!$products{$product}) {
+    printf STDERR "$0: Unknown product $product\n";
+    usage();
+}
+
+if (!@authors) { # No author supplied
+    @authors = ("AUTHOR"); # random author
+}
+
+if (!defined($dir)) {
+    $dir = tempdir("mathgen.$$.XXXXXXXXXXXXXXXXXXXXXXXX",
+		   TMPDIR => 1,
+		   CLEANUP => ($debug ? 0 : 1))
+	or die("tempdir: $!");
+    if ($debug) {
+	print STDERR "dir = $dir\n";
+    } 
+}
+
+my $output_fh;
+
+if (defined($output)) {
+    if ($output eq '-') {
+	$output_fh = *STDOUT;
+    } else {
+	open($output_fh, ">$output")
+	    or die("$output: $!");
+    }
+} else {
+    if ($mode eq 'pdf' or $mode eq 'zip' or $mode eq 'raw') {
+	print STDERR "$0: Must specify --output with --mode pdf,zip,raw\n";
+	usage();
+    }
+}
+
+if (defined($seed)) {
+    srand($seed);
+} else {
+    # In 5.14 srand returns seed value
+    if (0 and $^V and $^V ge v5.14) { # disabled until it can be tested
+	$seed = srand();
+    } else { # backward compatible
+	$seed = int rand 0xffffffff;
+	srand($seed);
+    }
+    if ($debug) {
+	print STDERR "seed = $seed\n";
+    }
+}
+
+# Blurb mode is a hack because the output is text not tex
+if ($product eq 'blurb' and $mode ne 'raw') {
+    printf STDERR "$0: --product=blurb only works with --mode=raw";
+    usage();
+}
+
+# Open rule file
+my $rulefile = "${data_dir}/sci${product}.in";
+my $rule_fh;
+open($rule_fh, "<$rulefile")
+    or die("$rulefile: $!");
+
+my $rules = {};
+my $rules_RE = undef;
+
+# add predefined rules
+$rules->{"AUTHOR_NAME"} = \@authors;
+
+{
+    my $s = "";
+    my @a = @authors;
+    my $la = pop(@a);
+    if (@a) {
+	$s = join(', ', @a) . ' and ';
+    }
+    $s .= $la;
+    $rules->{"SCIAUTHORS"} = [ $s ];
+}
+
+$rules->{"SEED"} = [ $seed ];
+
+scigen::read_rules ($rule_fh, $rules, \$rules_RE, $debug);
+my $text = scigen::generate (
+    $rules, 'START', $rules_RE, $debug, $products{$product}{PRETTY});
+
+if ($mode eq 'raw') {
+    print $output_fh $text;
+    exit 0; # why does indent screw up here?
+}
+
+sub generate_bibtex;
+sub dump_to_file;
+sub pdflatex;
+sub bibtex;
+sub makeindex;
+sub output_filespec;
+sub dump_to_file;
+my $basename = "mathgen-$seed";
+
+my $article_readme_text = <<"EOF";
+To recompile this file, run:
+
+pdflatex $basename
+bibtex $basename
+pdflatex $basename
+pdflatex $basename
+
+You need the following packages installed:
+
+AMS-LaTeX
+fullpage
+mathrsfs
+natbib
+truncate
+EOF
+    ; # fix indentation for emacs
+
+my $book_readme_text = <<"EOF";
+To recompile this file, run:
+
+pdflatex $basename
+bibtex $basename
+makeindex $basename.idx
+pdflatex $basename
+pdflatex $basename
+
+You need the following packages installed:
+
+AMS-LaTeX
+geometry
+mathrsfs
+natbib
+txfonts
+hyphenat
+textcase
+hyperref
+truncate
+titlesec
+makeidx
+url
+tocbibind
+
+The output is set to 6x9 inch paper and is suitable for lulu.com.
+EOF
+    ;
+
+my %readme_text = (
+    'article' => $article_readme_text,
+    'book' => $book_readme_text
+    );
+
+
+chdir($dir) or die("$dir: $!");
+dump_to_file($text, "$basename.tex");
+dump_to_file(generate_bibtex($text), $bibname);
+
+pdflatex($basename);
+bibtex($basename);
+pdflatex($basename);
+pdflatex($basename);
+
+# Now just dispose of the output appropriately
+
+if ($mode eq 'pdf') {
+    output_filespec("<$basename.pdf", "$basename.pdf");
+} elsif ($mode eq 'zip') {
+    dump_to_file($readme_text{$product}, 'README');
+    # Useless Use Of Cat issue: we could have zip write to the 
+    # output directly.  But we already opened it, and I don't feel like
+    # special casing that for --mode=zip.  Also trying to pass the output
+    # filename in a shell command seems dicey.
+    output_filespec("zip - $basename.tex $basename.pdf $bibname README |",
+		    "zip");
+} elsif ($mode eq 'view') {
+    system("$viewer $basename.pdf");
+} elsif ($mode eq 'dir') {
+    # Nothing to do here!
+}
+
+# We need to not be in the temp directory if it's going to be deleted
+if ($debug) {
+    print STDERR "dir = $dir, seed = $seed\n";
+}
+chdir('/');
+exit 0;
+
+# Subroutines follow
+
+sub output_filespec {
+    my ($filespec, $filename) = @_;
+    local $/ = \65536;
+    open (my $fh, $filespec)
+	or die("$filename: $!");
+    while (<$fh>) {
+	print $output_fh $_;
+    }
+    close($fh) 
+	or die("$filename: $!");
+}
+
+sub dump_to_file {
+    my ($text, $filename) = @_;
+    open (my $fh, ">$filename")
+	or die("$filename: $!");
+    print $fh $text;
+    close($fh)
+	or die("$filename: $!");
+}
+
+
+sub generate_bibtex {
+    my ($text) = @_;
+    my $bib;
+    my %citelabels = ();
+    while( $text =~ /(cite\:\d+)[,\}]/gi ) {
+	$citelabels{$1} = 1;
+    }
+    foreach my $clabel (keys(%citelabels)) {
+	$rules->{"CITE_LABEL_GIVEN"} = [ $clabel ];
+	scigen::compute_re($rules, \$rules_RE); # seems inefficient
+	$bib .= scigen::generate 
+	    ($rules, "BIBTEX_ENTRY", $rules_RE, $debug, 'bibtex');
+	$bib .= "\n";
+    }
+    return $bib;
+}
+
+sub pdflatex {
+    my ($base) = @_;
+    system("pdflatex -halt-on-error $base " . '1>&2')
+	and die("pdflatex failed");
+}
+
+sub bibtex {
+    my ($base) = @_;
+    system("bibtex $base " . '1>&2')
+	and die("bibtex failed");
+}
+
+sub makeindex {
+    my ($base) = @_;
+    system("makeindex $base.idx " . '1>&2')
+	and die("makeindex failed");
+}
---
+++ scigen.pm
@@ -0,0 +1,397 @@
+package scigen;
+
+#    This file is part of SCIgen.
+#
+#    SCIgen is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    SCIgen is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with SCIgen; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+use strict;
+use IO::File;
+use Data::Dumper;
+#use Autoformat;
+use vars qw($SCIGEND_PORT);
+
+#### daemon settings ####
+$SCIGEND_PORT = 4724;
+
+sub dup_name {
+    my $name = shift;
+    return $name . "!!!";
+}
+
+sub file_name {
+    my $name = shift;
+    return $name . ".file";
+}
+
+sub read_rules {
+    my ($fh, $rules, $RE, $debug) = @_;
+    my $line;
+    while ($line = <$fh>) {
+	next if $line =~ /^#/ ;
+	next if $line !~ /\S/ ;
+
+	my @words = split /\s+/, $line;
+	my $name = shift @words;
+	my $rule = "";
+
+	# non-duplicate rule
+	if( $name =~ /([^\+]*)\!$/ ) {
+	    $name = $1;
+	    push @{$rules->{dup_name("$name")}}, "";
+	    next;
+	}
+
+	# include rule
+	if( $name =~ /\.include$/ ) {
+	    my $file = $words[0];
+	    # make sure we haven't already included this file
+	    # NOTE: this allows the main file to be included at most twice
+	    if( defined $rules->{&file_name($file)} ) {
+		if( $debug > 0 ) {
+		    print "Skipping duplicate included file $file\n";
+		}
+		next;
+	    } else {
+		$rules->{&file_name($file)} = 1;
+	    }
+	    if( $debug > 0 ) {
+		print "Opening included file $file\n";
+	    }
+	    my $inc_fh = new IO::File ("<$file");
+	    if( !defined $inc_fh ) {
+		die( "Couldn't open included file $file" );
+	    }
+	    &read_rules( $inc_fh, $rules, undef, $debug );
+	    next; # we don't want to have .include itself be a rule
+	}
+
+	if ($#words == 0 && $words[0] eq '{') {
+	    my $end = 0;
+	    while ($line = <$fh>) {
+		if ($line =~ /^}[\r\n]+$/) {
+		    $end = 1;
+		    last;
+		} else {
+		    $rule .= $line;
+		}
+	    }
+	    if (! $end) {
+		die "$name: EOF found before close rule\n";
+	    }
+	} else {
+	    $line =~ s/^\S+\s+//; 
+	    chomp ($line);
+	    $rule = $line;
+	}
+
+	# look for the weight
+	my $weight = 1;
+	if( $name =~ /([^\+]*)\+(\d+)$/ ) {
+	    $name = $1;
+	    $weight = $2;
+	    if( $debug > 10 ) {
+		warn "weighting rule by $weight: $name -> $rule\n";
+	    }
+	}
+
+	do {
+	    push @{$rules->{$name}}, $rule;
+	} while( --$weight > 0 );
+    }
+
+    if( defined $RE ) {
+	compute_re( $rules, $RE );
+    }
+
+}
+
+sub compute_re {
+
+    # must sort; order matters, and we want to make sure that we get
+    # the longest matches first
+    my ($rules, $RE) = @_;
+    my $in = join "|", sort { length ($b) <=> length ($a) } keys %$rules;
+    $$RE = qr/^(.*?)(${in})/s ;
+
+}
+
+sub generate {
+    my ($rules, $start, $RE, $debug, $pretty) = @_;
+
+
+    my $s = expand ($rules, $start, $RE, $debug);
+    if($pretty =~ /latex/) {
+	$s = pretty_print_latex($s, $pretty, $debug);
+    } elsif ($pretty eq 'bibtex') {
+	$s = pretty_print_bibtex($s);
+    }
+    return $s;
+}
+
+sub pick_rand {
+    my ($set) = @_;
+    my $n = $#$set + 1;
+    my $v =  @$set[int (rand () * $n)];
+    return $v;
+}
+
+sub pop_first_rule {
+    my ($rules, $preamble, $input, $rule, $RE) = @_;
+
+    $$preamble = undef;
+    $$rule = undef;
+
+    my $ret = undef;
+    
+    if ($$input =~ s/$RE//s ) {
+	$$preamble = $1;
+	$$rule = $2;
+	return 1;
+    }
+	
+    return 0;
+}
+
+sub pretty_print_latex {
+
+    my ($s, $pretty, $debug) = @_;
+
+    my $news = "";
+    my @lines = split( /\n/, $s );
+    foreach my $line (@lines) {
+
+	$news .= "% $line\n" if ($debug);
+	my $newline = "";
+
+#	$line =~ s/(\s+)([\.\,\?\;\:])/$2/g;
+	$line =~ s/(\b)(a)((?:\s+)(?:\\[^\s\{]+\{)?(?:[aeiou]))/$1$2n$3/gi; # fix "a object"
+
+	if( $line =~ /(\\
+                       (?:sci)?
+                       (?:
+                          (?:
+                             (?:sub)?section\*?
+                          )
+                         |
+                          (?:chapter)
+                         |
+                          (?:title)
+                       )
+                      )
+                      {(.*)}/x ) {
+	    my ($command, $title) = ($1, $2);
+	    chomp $title;
+	    chomp $title;
+	    $title = my_entitle($title);
+	    if ($pretty eq 'latexbook') {
+	     	#my $short_title = ($title =~ /^(.{35}\S*)/) ? "$1\\dots" : $title;	     	
+		my $short_title = "\\truncate{0.75\\textwidth}{${title}}";
+	     	if ($command =~ /title/) {
+	     	    $newline = "${command}{${title}}";
+	     	} elsif ($command =~ /chapter/) {
+	     	    $newline = "${command}{${title}}\n\\chaptermark{${short_title}}";
+	     	} else {
+	     	    $newline = "${command}[${short_title}]{${title}}"
+	     	}
+	    } else {
+	    $newline = "${command}{${title}}";
+	    }
+	} elsif( $line =~ /\\index/ ) {
+	    $newline = $line; # don't mangle index entries
+	} elsif( $line =~ /\S/ ) {
+	    $newline = my_ensentence($line);
+	}
+
+	$newline =~ s/\\Em/\\em/g;
+
+	$newline =~ s/\s+([,.\-!?\';:])/$1/g; # fix "foo , bar"
+	$newline =~ s/-\s+/-/g; # fix "foo- bar"
+
+	# Replace "so then $$x=y$$." with "so then $$x=y.$$"
+	$newline =~ s/(\$\$|\\end\{align\*\})\s*([,.;:!?])/$2$1/g;
+
+	if( $newline !~ /\n$/ ) {
+	    $newline .= "\n";
+	}
+	$news .= $newline;
+
+    }
+
+    return $news;
+}
+
+sub pretty_print_bibtex {
+    my @lines = split( /\n/, $_[0] );
+    my $out;
+    
+    foreach my $line (@lines) {
+	if ($line =~ /^(.*(?:title|journal)\s*=\s*\{)(.*)(\},\s*)$/) {
+	    my ($first, $title, $last) = ($1,$2,$3);
+	    $title =~ s/(\b)(a)\s+([aeiou])/$1$2n $3/gi; #fix "a object"
+	    $title =~ s/\s+([,.\-!?\';:])/$1/g; # fix "foo , bar"
+	    $title =~ s/-\s+/-/g; # fix "foo- bar"
+	    $title = enbracket($title);
+	    $title = my_entitle($title);
+	    $line = $first . $title . $last;
+	}
+	$out .= $line . "\n";
+    }
+    return $out;
+}
+
+# Protect caps, etc, with brackets
+sub enbracket {
+    my $s = $_[0];
+    $s =~ s/(^|[\s\-])([[:upper:]])/$1\{$2\}/g;
+    $s =~ s/(\\[[:alpha:]]+(?:\{[^\s\}]*\})?)/\{$1\}/g;
+    return $s;
+}
+
+my %smallwords = map {$_=>1} qw {
+        a an at as and are
+        but by 
+        ere
+        for from
+        in into is
+        of on or over
+        per
+        the to that than
+        until unto upon
+        via
+        with while whilst within without
+};
+
+
+
+sub my_entitle {
+    my ($s) = @_;
+    my $out;
+    my @words = split(/([\s\-]+)/, $s);
+    foreach my $i (0..@words-1) {
+	my $w = $words[$i];
+	if ($w =~ /^[[:lower:]]/ &&
+	    ($i == 0 || $i == @words-1 || !$smallwords{$w})) {
+	    $out .= ucfirst($w);
+	} else {
+	    $out .= $w;
+	}
+    }
+    return $out;
+}
+
+sub my_ensentence {
+    my ($s) = @_;
+    my $out;
+    my @words = split(/([\s\-]+)/, $s);
+    my $start = 1;
+    foreach my $i (0..@words-1) {
+	my $w = $words[$i];
+# allow $ because a sentence could start with math
+	if ($w =~ /^[\$[:alpha:]]/ && $start) { 
+	    $out .= ucfirst($w);
+	    $start = 0;
+	} else {
+	    $out .= $w;
+	}
+	$start = 1 if ($w =~ /[.?!]$/);
+    }
+    return $out;
+}
+
+sub break_latex($$$) {
+    my ($text, $reqlen, $fldlen) = @_;
+    if( !defined $text ) {
+	$text = "";
+    }
+    return { $text, "" };
+}
+
+sub expand {
+    my ($rules, $start, $RE, $debug) = @_;
+
+    # check for special rules ending in + and # 
+    # Rules ending in + generate a sequential integer
+    # The same rule ending in # chooses a random # from among preiously
+    # generated integers
+    if( $start =~ /(.*)\+$/ ) {
+	my $rule = $1;
+	my $i = $rules->{$rule};
+	if( !defined $i ) {
+	    $i = 0;
+	    $rules->{$rule} = 1;
+	} else {
+	    $rules->{$rule} = $i+1;
+	}
+	return $i;
+    } elsif( $start =~ /(.*)\#$/ ) {
+	my $rule = $1;
+	my $i = $rules->{$rule};
+	if( !defined $i ) {
+	    $i = 0;
+	} else {
+	    $i = int rand $i;
+	}
+	return $i;
+    }
+    my $full_token;
+
+    my $repeat = 0;
+    my $count = 0;
+    do {
+
+	my $input = pick_rand ($rules->{$start});
+	$count++;
+	if ($debug >= 5) {
+	    warn "$start -> $input\n";
+	}
+
+	my ($pre, $rule);
+	my @components;
+	$repeat = 0;	
+
+	while (pop_first_rule ($rules, \$pre, \$input, \$rule, $RE)) {
+	    my $ex = expand ($rules, $rule, $RE, $debug);
+	    push @components, $pre if length ($pre);
+	    push @components, $ex if length ($ex);
+	}
+	push @components, $input if length ($input);
+	$full_token = join "", @components ;
+	my $ref = $rules->{dup_name("$start")};
+	if( defined $ref ) {
+	    my @dups = @{$ref};
+	    # make sure we haven't generated this exact token yet
+	    foreach my $d (@dups) {
+		if( $d eq $full_token ) {
+		    $repeat = 1;
+		}
+	    }
+	    
+	    if( !$repeat ) {
+		push @{$rules->{dup_name("$start")}}, $full_token;
+	    } elsif( $count > 50 ) {
+		$repeat = 0;
+	    }
+	    
+	}
+
+    } while( $repeat );
+
+    return $full_token;
+    
+}
+
+
+1;
